bdf2a3f8261c9d7adb0d119e3076b36c54f30864
Rochmat-Ali <72842838+Rochmat-Ali@users.noreply.github.com>
2021-01-12 17:28:19 +0700

    Update 1617051094.pl

--- 1617051094.pl
+++ 1617051094.pl
@@ -1,12 +1,35 @@
-child(john,sue).   child(john,sam).   
-child(jane,sue).   child(jane,sam).   
-child(sue,george).   child(sue,gina). 
+%keluarga berjenis kelamin laki-laki
+laki(warjito).
+laki(ali).
+laki(santoni).
+laki(rohmat).
 
-male(john).   male(sam).     male(george). 
-female(sue).  female(jane).  female(june). 
+%keluarga berjenis kelamin perempuan
+perempuan(lina).
+perempuan(haliana).
+perempuan(rosada).
 
-parent(Y,X) :- child(X,Y).
-father(Y,X) :- child(X,Y), male(Y).
-opp_sex(X,Y) :- male(X), female(Y). 
-opp_sex(Y,X) :- male(X), female(Y). 
-grand_father(X,Z) :- father(X,Y), parent(Y,Z).
+%orang tua dari masing-masing anak
+orangtua(ali,santoni).
+orangtua(haliana,santoni).
+orangtua(warjito,rosada).
+orangtua(lina,rosada).
+
+orangtua(santoni,rohmat).
+orangtua(santoni,rohmat).
+orangtua(rosada,rohmat).
+orangtua(rosada,rohmat).
+
+%query untuk mencari anggota keluarga dalam silsilah
+ibu(X,Y):-orangtua(X,Y),perempuan(X).
+ayah(X,Y):-orangtua(X,Y),laki(X).
+
+anak(X,Y):-orangtua(Y,X).
+
+suami(X,Y):-anak(P,X),anak(P,Y),laki(X).
+istri(Y,X):-anak(P,X),anak(P,Y),perempuan(Y).
+
+kakek(X,Z):-orangtua(X,Y),orangtua(Y,Z),laki(X).
+nenek(X,Z):-orangtua(X,Y),orangtua(Y,Z),perempuan(X).
+
+cucu(X,Z):-orangtua(Z,Y),anak(X,Y).
